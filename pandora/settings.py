b59efd1ca87bf242078eb2512ff4376786ed156b
Pedro Arroz <pbserra@gmail.com>
2023-06-20 09:23:42 +0100

    update dockerfile

--- pandora/settings.py
+++ pandora/settings.py
@@ -24,7 +24,7 @@ except ImportError:
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 INSTALLED_APPS = [
